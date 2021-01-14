c0b891941f050fe8f0c89c146c2c996928fd8cbb
hallanlennon <77417805+hallanlennon@users.noreply.github.com>
2021-01-13 20:04:21 -0400

    Adicionando arquivos via upload
    Adicioando arquivos inicial do projeto.

---
+++ musicstore-api/src/main/java/com/app/musicstore/api/MusicstoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.musicstore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicstoreApiApplication.class, args);
+	}
+
+}
